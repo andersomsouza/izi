fab33bccd5a9b6082573f6f39a6006cd573f0514
Andersom Souza <andersom.eugenio@gmail.com>
2018-02-07 01:51:51 -0200

    finalizado add layout vue

--- resources/assets/js/components/dashboard/Home.vue
+++ resources/assets/js/components/dashboard/Home.vue
@@ -5,15 +5,18 @@
                 <h1>Adicionar Dispositivos</h1>
                 <div class="divider" style="width:30%;margin-left:35%; height:2px ;background-color:#29b7b2"></div>
             </div>
-            <div class="col offset-s3 s6" style="height:7rem; margin-top:10px;padding: 17px 0px 0px 50px; border:3px solid #29b7b2">
+            <div class="col offset-s3 s6"
+                 style="height:7rem; margin-top:10px;padding: 17px 0px 0px 50px; border:3px solid #29b7b2">
                 <div class="row">
                     <form class="col s12">
                         <div class="row">
                             <div class="input-field col s8">
                                 <input placeholder="ID" id="first_name" type="text" class="validate">
-                                <label class="active" for="first_name" style="text-transform:uppercase">Identificador do Dispositivo</label>
+                                <label class="active" for="first_name" style="text-transform:uppercase">Identificador do
+                                    Dispositivo</label>
                             </div>
-                            <a class="btn-floating btn-medium" style="position: relative;margin-left:3rem;margin-top:51px;width:50px; height:50px">
+                            <a class="btn-floating btn-medium"
+                               style="position: relative;margin-left:3rem;margin-top:51px;width:50px; height:50px">
                                 <i class="large material-icons" style="font-size: 2rem;line-height: 50px;">add</i>
                             </a>
                         </div>
@@ -40,13 +43,16 @@
                                     <div class="row" style="margin-bottom:0px">
                                         <div class="input-field col s6">
                                             <input placeholder="Tipo" id="tipo" type="text" class="validate">
-                                            <label class="active" for="tipo" style="text-transform:uppercase">Identificador do Dispositivo</label>
+                                            <label class="active" for="tipo" style="text-transform:uppercase">Identificador
+                                                do Dispositivo</label>
                                         </div>
                                         <div class="input-field col s6">
                                             <input placeholder="Topico" id="topico" type="text" class="validate">
-                                            <label class="active" for="topico" style="text-transform:uppercase">Identificador do Dispositivo</label>
+                                            <label class="active" for="topico" style="text-transform:uppercase">Identificador
+                                                do Dispositivo</label>
                                         </div>
-                                        <a class="btn-floating btn-medium" style="position: relative;margin-left:45rem;margin-top:-10px;width:50px; height:50px">
+                                        <a class="btn-floating btn-medium"
+                                           style="position: relative;margin-left:45rem;margin-top:-10px;width:50px; height:50px">
                                             <i class="large material-icons" style="font-size: 2rem;line-height: 50px;">add</i>
                                         </a>
                                     </div>
@@ -116,10 +122,33 @@
 
 <script>
     export default {
-        name: "home"
+        name: "home",
+        mounted() {
+            $('ul.tabs').tabs()
+        }
     }
 </script>
 
 <style scoped>
 
+    .box-primary-create{
+        height:10rem;
+        margin-top:70px;
+        padding: 40px 0px 0px 30px;
+        border:3px solid #4e8180;
+    }
+
+    .tabs .tab a:hover, .tabs .tab a.active {
+        background-color: transparent;
+        color: #4e8180;
+    }
+
+    .tabs .indicator {
+        background-color: #4e8180;
+    }
+
+    .tab-slide{
+        height: 40vh;
+        margin-top: 15px;
+    }
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Laravel</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
         <style>
             html, body {
                 background-color: #fff;
